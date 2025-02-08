8a55d6e71e66f7bc99e9ebe55ff4209abefb5506
kyungjung kim <kj.kim@live.co.kr>
2025-02-09 06:19:40 +0900

    Add files via upload

---
+++ QR Scanner/QR Scanner/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  QR Scanner
+//
+//  Created by Kyungjung Kim on 2023/02/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ QR Scanner/QR ScannerTests/QR_ScannerTests.m
@@ -0,0 +1,36 @@
+//
+//  QR_ScannerTests.m
+//  QR ScannerTests
+//
+//  Created by Kyungjung Kim on 2023/02/13.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface QR_ScannerTests : XCTestCase
+
+@end
+
+@implementation QR_ScannerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
