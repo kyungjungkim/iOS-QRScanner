d2625a91d0fb64aab93943a8ea0f36f56b7caaea
kyungjung kim <kj.kim@live.co.kr>
2025-02-27 17:41:02 +0900

    Add files via upload

---
+++ QR Scanner/QR Scanner/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  QR Scanner
+//
+//  Created by Kyungjung Kim on 2023/02/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
